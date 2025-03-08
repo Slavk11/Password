48e2e7b9c86c0a50d11cddba007fa5f64fd8f112
Stanislav Sazonov <slavk11@yandex.ru>
2025-03-08 23:34:17 +0300

    Added constructor func Added data validation

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
+	"net/url"
 )
 
 type account struct {
@@ -23,26 +25,46 @@ func (acc *account) generatePassword(n int) {
 	acc.password = string(res)
 }
 
+func newAccount(login, password, urlString string) (*account, error) {
+	if login == "" {
+		return nil, errors.New("INVALID_LOGIN")
+	}
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+	newAcc := &account{
+		login,
+		password,
+		urlString,
+	}
+	if password == "" {
+		newAcc.generatePassword(12)
+	}
+	return newAcc, nil
+
+}
+
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
 
 func main() {
 
 	login := promptData("Введите логин")
-	//password := promptData("Введите пароль")
+	password := promptData("Введите пароль")
 	url := promptData("Введите URL")
 
-	account1 := account{
-		login: login,
-		url:   url,
+	myAccount, err := newAccount(login, password, url)
+	if err != nil {
+		fmt.Println("Неверный формат URL или Логин")
+		return
 	}
-	account1.generatePassword(12)
-	account1.outputPassword()
-	fmt.Println(account1)
+	myAccount.outputPassword()
+	fmt.Println(myAccount)
 }
 
 func promptData(prompt string) string {
 	fmt.Print(prompt + ": ")
 	var res string
-	fmt.Scan(&res)
+	fmt.Scanln(&res)
 	return res
 }
